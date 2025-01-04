4c1b8d4a918a75f9bb33b8f6fe18a99cf05f47d2
kuzowebsite <129853786+kuzowebsite@users.noreply.github.com>
2025-01-04 14:24:12 +0800

    Add files via upload

---
+++ loft.js
@@ -0,0 +1,58 @@
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0)
+});
+
+//////////// INVESTMENT BUTTONS////////////////////////////////
+
+const about = document.querySelector(".about");
+const btns = document.querySelectorAll(".tab-btn");
+const articles = document.querySelectorAll(".content");
+about.addEventListener("click", function (e) {
+const id = e.target.dataset.id;
+if (id) {
+btns.forEach(function (btn) {
+btn.classList.remove("active");
+});
+e.target.classList.add("active");
+
+articles.forEach(function (article) {
+article.classList.remove("active");
+});
+const element = document.getElementById(id);
+element.classList.add("active");
+}
+});
+
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+  const btn = question.querySelector(".question-btn");
+  // console.log(btn);
+
+  btn.addEventListener("click", function () {
+    // console.log(question);
+
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+     
+      }
+    });
+
+    question.classList.toggle("show-text");
+   
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const roso = document.getElementById('loader');
+    const rosoh = document.querySelector('.rosoh');
+
+    setTimeout(function(){
+           rosoh.remove();
+       }, 4000);
+    setTimeout(function(){
+           roso.remove();
+       }, 6000);
+   });
